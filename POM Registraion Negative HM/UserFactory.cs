d2e473864d9a30e616095723b36c5e1cb4db3db1
Ivaylo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-10-07 17:58:53 +0300

    Add files via upload

---
+++ POM Registraion Negative HM/UserFactory.cs
@@ -0,0 +1,24 @@
+﻿namespace Homework
+{
+    public static class UserFactory
+    {
+        public static RegistrationUser CreateValidUser()
+        {
+            return new RegistrationUser
+            {
+                FirstName = "Joro",
+                LastName = "Doev",
+                Date = "3",
+                Month = "3",
+                Year = "1985",
+                Address = "Vitoshka Str",
+                City = "Sofia",
+                State = "Arizona",
+                Password = "passss",
+                PostCode = "50050",
+                Phone = "087654321",
+                Alias = "Home"
+            };
+        }
+    }
+}
