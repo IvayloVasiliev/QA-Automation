c4a0b3b2b7421a3debb0e89ed455e013c7e23744
Ivaylo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-10-11 17:17:39 +0300

    Add files via upload

---
+++ POMHomework/QAAutomation/QAAutomationAssert.cs
@@ -0,0 +1,40 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.IO;
+using System.Reflection;
+
+namespace POMHomework.QAAutomation
+{
+    [TestFixture]
+   public class QAAutomationAssert
+    {
+        private ChromeDriver _driver;
+        private SUMainPage _sUMainPage;
+        private QACursPage _qACursPage;
+
+        [SetUp]
+        public void CalssInit()
+        {
+            _driver = new ChromeDriver(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location));
+            _driver.Manage().Window.Maximize();
+            
+            _sUMainPage = new SUMainPage(_driver);
+            _qACursPage = new QACursPage(_driver);
+        }
+
+        [Test]
+        public void QAAutomationCurs()
+        {
+            _sUMainPage.Navigate();
+           
+            Assert.AreEqual(_qACursPage.Result(), "QA Automation - септември 2019");
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _driver.Quit();
+        }
+    }
+}
---
+++ POMHomework/QAAutomation/QACursPage.cs
@@ -0,0 +1,20 @@
+﻿using OpenQA.Selenium;
+
+namespace POMHomework.QAAutomation
+{
+    public class QACursPage : QABasePage
+    {
+        public QACursPage(IWebDriver driver) : base(driver)
+        {
+        }
+
+        public IWebElement FoundResult => Driver.FindElement(By.XPath(@"/html/body/div[2]/header/h1"));
+
+
+        public string Result()
+        {
+            var foundResult = FoundResult.Text;
+            return foundResult;
+        }
+    }
+}
