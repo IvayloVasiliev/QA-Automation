1b3e6392e83a7d0e182deeeee3e45a8d76ee352b
Ivaylo Vasiliev <ivailo_vasiliev@hotmail.com>
2019-10-11 23:03:10 +0300

    Add files via upload

---
+++ POMHomework Final/GoogleSearch/GoogleSerach.Asserts.cs
@@ -0,0 +1,12 @@
+﻿using NUnit.Framework;
+
+namespace POMHomework.GoogleSearch
+{
+    public partial class GoogleSerach
+    {
+        public void FoundResult(string foundResult)
+        {
+            Assert.AreEqual("Selenium - Web Browser Automation", foundResult);
+        }
+    }
+}
---
+++ POMHomework Final/QAAutomation/QABasePage.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+
+namespace POMHomework.QAAutomation
+{
+    public abstract class QABasePage
+    {
+        private IWebDriver _driver;
+
+        public QABasePage(IWebDriver driver)
+        {
+            _driver = driver;
+        }
+
+        public IWebDriver Driver => _driver;
+
+        public virtual void Navigate(string url)
+        {
+            Driver.Url = url;
+        }
+
+    }
+}
